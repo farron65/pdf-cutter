ffb2bd9550286efd48e7aaa2d5f20a636afc7184
farron65 <bezhansn@gmail.com>
2026-02-24 22:48:04 -0500

    Inital setup: PDFBox wired and CLI working

---
+++ src/PDFDocument.java
@@ -0,0 +1,16 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.Loader;
+import java.io.File;
+import java.io.IOException;
+
+public class PDFDocument {
+
+    PDFDocument(String filePath, String pagesToCut) throws IOException {
+        this.pdfFile = Loader.loadPDF(new File(filePath));
+        this.pagesToCut = pagesToCut;
+    }
+    
+    private PDDocument pdfFile;
+    public String pagesToCut;
+
+}
